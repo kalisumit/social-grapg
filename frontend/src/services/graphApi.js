3777771d576055ce0435e491e1f12cd9e8a62196
kalisumit <sumit7314roy@gmail.com>
2026-06-05 03:31:00 +0530

    frontend

--- frontend/src/services/graphApi.js
+++ frontend/src/services/graphApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://social-graph.onrender.com/api";
 
 export const getGraph = async () => {
     const response = await axios.get(`${API_URL}/graph`)
--- frontend/src/services/userApi.js
+++ frontend/src/services/userApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/users"
+const API_URL = "https://social-graph.onrender.com/api/users"
 
 export const createUser = async (userData) => {
     const response = await axios.post(API_URL, userData);
